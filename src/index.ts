dc50ef9e4c54d2afa0f57ed9281144a701920f85
James Wenzel <james.wenzel@bridge.xyz>
2025-03-07 14:20:28 -0800

    add default export

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { createServer } from "./rust-docs.js";
+import { createServer } from "./rust-docs";
 
-const { server } = createServer();
-
-// Connect to the stdio transport and start the server
-server
-    .connect(new StdioServerTransport())
-    .then(() => {
-        console.error("MCP Crate Docs Server is running...");
-    })
-    .catch((err: unknown) => {
-        const errorMessage = err instanceof Error ? err.message : String(err);
-        console.error("Failed to start MCP server:", errorMessage);
-        process.exit(1);
-    });
+export default createServer;
---
+++ src/stdio.ts
@@ -0,0 +1,16 @@
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { createServer } from "./rust-docs.js";
+
+const { server } = createServer();
+
+// Connect to the stdio transport and start the server
+server
+    .connect(new StdioServerTransport())
+    .then(() => {
+        console.error("MCP Crate Docs Server is running...");
+    })
+    .catch((err: unknown) => {
+        const errorMessage = err instanceof Error ? err.message : String(err);
+        console.error("Failed to start MCP server:", errorMessage);
+        process.exit(1);
+    });
